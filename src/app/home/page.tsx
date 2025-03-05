be71b0e38ea13cb613cd4b1ef980dac5de3a2ef8
Anderson Andrade <Anderson Andrade>
2025-03-05 19:59:11 -0300

    refactor(home page): change description incricoes to assinaturas

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             <div className={'flex flex-col h-full'}>
                 <div className={'flex flex-col grow gap-3 justify-center items-center'}>
                     <Image src={homeImage} alt={'home-image'} width={400} height={400}/>
-                    <h3 className={'text-lg'}>Seja bem vindo! Aqui você pode gerenciar todas as suas incrições</h3>
+                    <h3 className={'text-lg'}>Seja bem vindo! Aqui você pode gerenciar todas as suas assinaturas</h3>
                 </div>
             </div>
         </PageTemplate>
